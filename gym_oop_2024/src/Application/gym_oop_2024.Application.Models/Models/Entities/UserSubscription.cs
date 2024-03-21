09abf7aecdff4e3dadcc528f49852a54f2310a09
Алексей Конопля <alexey.konoplia@gmail.com>
2024-03-21 20:56:32 +0300

    resolved entities

---
+++ gym_oop_2024/src/Application/gym_oop_2024.Application.Models/Models/Entities/UserSubscription.cs
@@ -0,0 +1,12 @@
+﻿namespace gym_oop_2024.Application.Models;
+
+public class UserSubscription
+{
+    public int UserSubscriptionId;
+    public int SubscriptionId; // foreign
+    public int UserId; // foreign
+    public bool IsActive;
+    public bool IsMulticard;
+    public DateTime StartDate;
+    public DateTime EndDate;
+}
